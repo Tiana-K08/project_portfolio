3bc8b22649baaf05e56fb341dd8334e94344c29f
Tiana-K08 <tetkononenko2020@gmail.com>
2025-02-10 18:15:40 +0100

    change mode

--- js/toggle_dark_light.js
+++ js/toggle_dark_light.js
@@ -1,5 +1,11 @@
 const buttonDarkLight = document.querySelector('.toggle-btn');
+const bodyPages = document.querySelector('body');
+const bgHomepage = document.querySelector('.homepage-wrapper');
+const nameShadow = document.querySelector('#name-shadow');
 
 buttonDarkLight.onclick = function() {
     buttonDarkLight.classList.toggle('toggle-btn--active');
+    bodyPages.classList.toggle('light-mode');
+    bgHomepage.classList.toggle('bg-img-light-mode');
+    nameShadow.classList.toggle('shadow-text-light-mode');
 }
