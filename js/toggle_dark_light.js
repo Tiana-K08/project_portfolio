d8dfc4785b36242dc6bb86545175226a700211e0
Tiana-K08 <tetkononenko2020@gmail.com>
2025-02-10 21:19:22 +0100

    localStorage dark-light mode

--- js/toggle_dark_light.js
+++ js/toggle_dark_light.js
@@ -3,9 +3,53 @@ const bodyPages = document.querySelector('body');
 const bgHomepage = document.querySelector('.homepage-wrapper');
 const nameShadow = document.querySelector('#name-shadow');
 
+function activeLightMode() {
+    if (buttonDarkLight) {
+        buttonDarkLight.classList.add('toggle-btn--active');
+    }
+    if (bodyPages) {
+        bodyPages.classList.add('light-mode');
+    }
+    if (bgHomepage) {
+        bgHomepage.classList.add('bg-img-light-mode');
+    }
+    if (nameShadow) {
+        nameShadow.classList.add('shadow-text-light-mode');
+    }
+}
+
+function activeDarkMode() {
+    if (buttonDarkLight) {
+        buttonDarkLight.classList.remove('toggle-btn--active');
+    }
+    if (bodyPages) {
+        bodyPages.classList.remove('light-mode');
+    }
+    if (bgHomepage) {
+        bgHomepage.classList.remove('bg-img-light-mode');
+    }
+    if (nameShadow) {
+        nameShadow.classList.remove('shadow-text-light-mode');
+    }
+} 
+
 buttonDarkLight.onclick = function() {
     buttonDarkLight.classList.toggle('toggle-btn--active');
-    bodyPages.classList.toggle('light-mode');
-    bgHomepage.classList.toggle('bg-img-light-mode');
-    nameShadow.classList.toggle('shadow-text-light-mode');
-}
+    const isLightMode = buttonDarkLight.classList.contains('toggle-btn--active');
+    if (isLightMode) {
+        activeLightMode();
+        localStorage.setItem('mode', 'light');
+    } else {
+        activeDarkMode();
+        localStorage.setItem('mode', 'dark');
+    }
+}  
+
+document.addEventListener("DOMContentLoaded", function() {
+    const savedMode = localStorage.getItem('mode');
+    if (savedMode === 'light') {
+        activeLightMode();
+    } else {
+        activeDarkMode();
+    }
+});
